5348e73d352dc5eef5e7c0e4856706ad096a5249
Chmara Vlad <chmaravlad5@gmail.com>
2022-06-23 15:10:24 +0300

    Add data and types

---
+++ src/types.d.ts
@@ -0,0 +1,17 @@
+// types
+export interface ISliderItem {
+     id: number,
+     img: string,
+     title: string,
+     desc: string,
+     bg: string,
+}
+export interface ICategory {
+     id: number,
+     img: string,
+     title: string,
+}
+export interface IPopularProduct {
+     id: number,
+     img: string,
+}
